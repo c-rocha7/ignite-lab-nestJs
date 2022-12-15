c57d313cb0b8f2e1288548b5f282389136f438ce
Cauã R. Pereira <pereira.cauaa7@gmail.com>
2022-12-15 10:52:42 -0300

    Retirado as importações do mail e acrescentado a do prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { HTTPModule } from './http.module';
-import { MailService } from './mail/mail.service';
-import { SMTPMailService } from './mail/smtp-mail.service';
+import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [HTTPModule],
   controllers: [AppController],
   providers: [
-    {
-      provide: MailService,
-      useClass: SMTPMailService,
-    },
+    PrismaService,
   ],
 })
 export class AppModule {}
