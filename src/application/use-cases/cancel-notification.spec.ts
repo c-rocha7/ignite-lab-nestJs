8c49a089c19242e8c1391d8d2b52196f6178448c
Cauã R. Pereira <pereira.cauaa7@gmail.com>
2022-12-16 21:20:42 -0300

    Criado toda a estruturada do cancel-notification

---
+++ src/application/use-cases/cancel-notification.spec.ts
@@ -0,0 +1,39 @@
+import { Content } from '@application/entities/content';
+import { Notification } from '@application/entities/notification';
+import { InMemoryNotificationsRepository } from '@test/repositories/in-memory-notifications-repository';
+import { CancelNotification } from './cancel-notification';
+import { NotificationNotFound } from './errors/notification-not-found';
+
+describe('Cancelamento de notificação', () => {
+  it('Teste para verificar o cancelamento da notificação', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const cancelNotification = new CancelNotification(notificationsRepository);
+
+    const notification = new Notification({
+      category: 'social',
+      content: new Content('Nova solicitação de amizade'),
+      recipientId: 'example-cancel-notification',
+    });
+
+    await notificationsRepository.create(notification);
+
+    await cancelNotification.execute({
+      notificationId: notification.id,
+    });
+
+    expect(notificationsRepository.notifications[0].canceledAt).toEqual(
+      expect.any(Date),
+    );
+  });
+
+  it('Teste para verificar se da erro ao tentar cancelar a notificação se não existir', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const cancelNotification = new CancelNotification(notificationsRepository);
+
+    expect(() => {
+      return cancelNotification.execute({
+        notificationId: 'example-fake-notification',
+      });
+    }).rejects.toThrow(NotificationNotFound);
+  });
+});
