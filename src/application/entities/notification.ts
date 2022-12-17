2978ab10736f728401920b99f3629b6e846bad6e
Cauã R. Pereira <pereira.cauaa7@gmail.com>
2022-12-17 02:47:23 -0300

    Implementando o repositório do prisma

--- src/application/entities/notification.ts
+++ src/application/entities/notification.ts
@@ -1,6 +1,6 @@
-import { Replace } from '@helpers/Replace';
-import { Content } from './content';
 import { randomUUID } from 'node:crypto';
+import { Replace } from '../../helpers/Replace';
+import { Content } from './content';
 
 export interface NotificationProps {
   recipientId: string;
@@ -15,8 +15,11 @@ export class Notification {
   private _id: string;
   private props: NotificationProps;
 
-  constructor(props: Replace<NotificationProps, { createdAt?: Date }>) {
-    this._id = randomUUID();
+  constructor(
+    props: Replace<NotificationProps, { createdAt?: Date }>,
+    id?: string,
+  ) {
+    this._id = id ?? randomUUID();
     this.props = {
       ...props,
       createdAt: props.createdAt ?? new Date(),
